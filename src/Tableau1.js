ed13e619c7fc7ebbf39d98e4116b03c5756586dd
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2021-10-25 15:55:26 +0200

    new montagne et arbre dernier plan

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -9,7 +9,9 @@ class Tableau1 extends Phaser.Scene{
     preload(){
         //bg 2 (tout au fond et très flou)
         this.load.image('bg2-terrain-2', 'assets/level/background-2/bg2-terrain-2.png');
+        this.load.image('bg2-terrain-4', 'assets/level/background-2/bg2-terrain-4.png');
         this.load.image('bg2-tree-2', 'assets/level/background-2/bg2-tree-2.png');
+        this.load.image('bg2-tree-3', 'assets/level/background-2/bg2-tree-3.png');
 
         //bg 1 (gris légèrement flou)
         this.load.image('bg1-terrain-3', 'assets/level/background-1/bg-terrain-3.png');
@@ -25,13 +27,14 @@ class Tableau1 extends Phaser.Scene{
             this.load.image('g-grass-'+i, 'assets/level/ground/g-grass-'+i+'.png');
         }
 
-        //filtre film TODO élève : faire une boucle à la place des 3 lignes qui suivent
+        //filtre film
         // TODO FAIT
-        for(let i=1;i<=5;i++) {
+        for(let i=1;i<=3;i++) {
             this.load.image('filterFilm'+i, 'assets/level/filters/film/frame-'+i+'.png');
         }
-        //texture au fond  TODO élève : faire une boucle pour charger les 3 images et démontrer par la même que vous savez aller au plus simple
-        for(let i=1;i<=5;i++) {
+        //texture au fond
+        // TODO FAIT
+        for(let i=1;i<=3;i++) {
             this.load.image('bg-animation-'+i, 'assets/level/background-2/bg-animation/bg-animation-'+i+'.png');
         }
     }
@@ -41,6 +44,7 @@ class Tableau1 extends Phaser.Scene{
      * TODO élèves : reproduire à l'identique assets/level/00-preview-example/sample1.jpg
      * TODO élèves : plus tard, continuez le décor vers la droite en vous servant des assets mis à votre disposition
      */
+
     create(){
 
         /**
@@ -61,15 +65,24 @@ class Tableau1 extends Phaser.Scene{
          * Terrain dans bg2
          * @type {Phaser.GameObjects.Image}
          */
-        let bg2Terrain2=this.add.image(-100,100, 'bg2-terrain-2').setOrigin(0,0);
+        // première montagne
+        let bg2Terrain2=this.add.image(-100,150, 'bg2-terrain-2').setOrigin(0,0);
         this.bg2Container.add(bg2Terrain2);
+        // deuxième montagne
+        let bg2Terrain4=this.add.image(650,200, 'bg2-terrain-4').setOrigin(0,0);
+        this.bg2Container.add(bg2Terrain4);
         /**
          * Arbre dans bg2
          * @type {Phaser.GameObjects.Image}
          */
-        let bg2Tree2=this.add.image(400,-50, 'bg2-tree-2').setOrigin(0,0);
+        let bg2Tree2=this.add.image(350,-50, 'bg2-tree-2').setOrigin(0,0);
         this.bg2Container.add(bg2Tree2);
-        bg2Tree2.angle=-5; //pencher l'arbre de -5 degrès
+        bg2Tree2.angle=0; //pencher l'arbre de tant degrès
+
+        //arbres montagne deux
+        let bg2Tree3=this.add.image(700,-60, 'bg2-tree-3').setOrigin(0,0);
+        this.bg2Container.add(bg2Tree3);
+        bg2Tree3.angle=-7; //pencher l'arbre de tant degrès
 
         //--------------background 1 (gris) --------------------
 
@@ -97,7 +110,7 @@ class Tableau1 extends Phaser.Scene{
          * @type {Phaser.GameObjects.Image}
          */
         let tree1=this.add.image(300,350, 'gTree1').setOrigin(0,1);
-        tree1.setTintFill(0xFF0000); // pratique pour dbugger A ENLEVER A LA FIN
+       // tree1.setTintFill(0xFF0000); // pratique pour dbugger A ENLEVER A LA FIN
         this.groundContainer.add(tree1);
         /**
          * Terrain 1
